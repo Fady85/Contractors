443f66ce6e3905ba782ae35b22f38b5527c31d4a
Fady <f.nader.nagy@gmail.com>
2024-05-21 13:01:27 +0300

    .

---
+++ contractor/contractor/doctype/contractor_invoice/test_contractor_invoice.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, cltd and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestContractorInvoice(FrappeTestCase):
+	pass
